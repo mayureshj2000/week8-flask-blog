80ddb68f165bd350143f2d69a6e347cf5d12d27c
mayureshj2000 <mj.ethical@gmail.com>
2026-03-20 09:10:39 +0000

    updates routes.py import werkzeug.urls

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import current_user, login_user, logout_user, login_required
-from werkzeug.urls import url_parse
+from urllib.parse import urlparse
 from app import db
 from app.models import User
 from app.auth import bp
@@ -19,7 +19,7 @@ def login():
             return redirect(url_for("auth.login"))
         login_user(user, remember=True)
         next_page = request.args.get("next")
-        if not next_page or url_parse(next_page).netloc != "":
+        if not next_page or urlparse(next_page).netloc != "":
             next_page = url_for("main.index")
         return redirect(next_page)
     return render_template("auth/login.html", form=form)
