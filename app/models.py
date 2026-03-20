7fa694c24fdd9b7e4f50b83c0242e1f70fbe13c4
mayureshj2000 <mj.ethical@gmail.com>
2026-03-20 09:40:53 +0000

    model.py Columns -> Column

--- app/models.py
+++ app/models.py
@@ -33,7 +33,7 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    published = db.Columns(db.Boolean, default=True)
+    published = db.Column(db.Boolean, default=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     comments = db.relationship("Comment", backref="post", lazy="dynamic",
     cascade="all, delete-orphan")
@@ -45,7 +45,7 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    approved = db.Columns(db.Boolean, default=False)
+    approved = db.Column(db.Boolean, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
     
