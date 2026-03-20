9f35319fb5a32930b0dfcd429acc1edf5bf73731
mayureshj2000 <mj.ethical@gmail.com>
2026-03-20 09:27:40 +0000

    fixed model.py

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login_manager
 
-class User(UserMixin, db.model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False, index=True)
     email = db.Columns(db.String(120), unique=True, nullable=False, index=True)
